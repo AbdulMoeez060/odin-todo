1754c768451abcbd467939a85e169f99f200846f
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-10-05 19:44:53 +0500

    remove unneeded file

--- src/generateJoke.js
+++
@@ -1,5 +0,0 @@
-function generateJoke(){
-    return "i don't like stairs \nThe are always upto something...";
-}
-
-export default generateJoke;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import generateJoke from "./generateJoke";
 import './styles/main.css';
 
-console.log(generateJoke());
+console.log('generateJoke');
