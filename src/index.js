d17182088ad6dc3f192e9dd00bf80bd08a9c6f6e
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-10-06 19:32:38 +0500

    add sidebar elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import "./styles/main.css";
 import hamburger from "./assets/hamburger.png";
 import todo from "./assets/todo.png";
+import tasks from "./assets/tasks.png";
+import today from "./assets/today.png";
+import week from "./assets/week.png";
+
+
