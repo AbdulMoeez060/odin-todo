bde6b5af45ee8aaeca2764c640089628833db1de
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-10-06 22:55:08 +0500

    add project name styling

--- src/index.js
+++ src/index.js
@@ -5,3 +5,5 @@ import tasks from "./assets/tasks.png";
 import today from "./assets/today.png";
 import week from "./assets/week.png";
 import add from "./assets/add.png";
+import del from "./assets/del.png";
+import drag from "./assets/drag.png";
