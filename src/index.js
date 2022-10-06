6e6be7e70d40d65c9c0f5f9e247905368f73b683
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-10-06 19:08:34 +0500

    add prettier

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import './styles/main.css';
-import hamburger from './assets/hamburger.png';
-import todo from './assets/todo.png'
+import "./styles/main.css";
+import hamburger from "./assets/hamburger.png";
+import todo from "./assets/todo.png";
