6ba66d7e1702b6bab8c0446cea0e53e60aa9b045
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-10-08 00:30:39 +0500

    refactor

--- src/index.js
+++ src/index.js
@@ -8,88 +8,77 @@ import add from "./assets/add.png";
 import del from "./assets/del.png";
 import drag from "./assets/drag.png";
 
+const displayController = (() => {
+  addEvents();
+  function addEvents() {
+    var menu = document.querySelector(".hamburger");
+    var addProjectButton = document.querySelector(".add-project");
+    var cancelProject = document.querySelector(".cancel-project");
 
-const displayController = (()=>{
+    var tasksButtons = document.querySelectorAll(".btn");
 
-    addEvents();
-    function addEvents() {
-        var menu = document.querySelector('.hamburger');
-        var addProjectButton = document.querySelector('.add-project');
-        var cancelProject = document.querySelector('.cancel-project');
+    var addTodoButton = document.querySelector(".add-todo");
+    var cancelTodo = document.querySelector(".cancel-todo");
 
-        var tasksButtons = document.querySelectorAll('.btn');
+    menu.addEventListener("click", clickMenu);
+    addProjectButton.addEventListener("click", toggleProjectForm);
+    cancelProject.addEventListener("click", toggleProjectForm);
 
-        var addTodoButton = document.querySelector('.add-todo');
-        var cancelTodo = document.querySelector('.cancel-todo');
+    addTodoButton.addEventListener("click", toggleTodoForm);
+    cancelTodo.addEventListener("click", toggleTodoForm);
 
+    tasksButtons.forEach((btn) => btn.addEventListener("click", changeTaskTab));
+  }
 
-        menu.addEventListener('click',clickMenu);
-        addProjectButton.addEventListener('click',toggleProjectForm);
-        cancelProject.addEventListener('click',toggleProjectForm);
+  function clickMenu() {
+    var sidebar = document.querySelector(".sidebar");
+    sidebar.classList.toggle("hide");
+  }
 
-        addTodoButton.addEventListener('click',toggleTodoForm)
-        cancelTodo.addEventListener('click',toggleTodoForm)
-
-
-        tasksButtons.forEach(btn => btn.addEventListener('click',changeTaskTab));
+  function changeTaskTab(e) {
+    var btns = document.querySelectorAll(".btn");
+    btns.forEach((btn) => btn.classList.remove("active"));
+    e.target.classList.add("active");
+    var heading = document.querySelector(".name");
+    if (e.target.classList.contains("all-tasks")) {
+      heading.innerText = "All Tasks";
+    } else if (e.target.classList.contains("today")) {
+      heading.innerText = "Today's Tasks";
+    } else if (e.target.classList.contains("week")) {
+      heading.innerText = "This Week's Tasks";
     }
+  }
 
+  function toggleProjectForm(e) {
+    if (e.target.classList.contains("add-project")) {
+      e.target.classList.add("hide");
+      var form = document.querySelector(".project-form");
+      form.classList.remove("hide");
+    } else if (e.target.classList.contains("cancel-project")) {
+      var form = document.querySelector(".project-form");
 
-    function clickMenu(){
-        var sidebar = document.querySelector('.sidebar')
-        sidebar.classList.toggle('hide');
+      form.classList.add("hide");
+      var input = document.querySelector(".project-name-inp");
+      input.value = "";
+      var projectButton = document.querySelector(".add-project");
+
+      projectButton.classList.remove("hide");
     }
+  }
 
-    function changeTaskTab(e){
-        var btns = document.querySelectorAll('.btn')
-        btns.forEach(btn=> btn.classList.remove('active'));
-        e.target.classList.add('active');
-        var heading = document.querySelector('.name');
-        if (e.target.classList.contains('all-tasks')) {
-            heading.innerText = 'All Tasks'
-        }
-        else if (e.target.classList.contains('today')) {
-            heading.innerText = "Today's Tasks"
-            
-        }
-        else if (e.target.classList.contains('week')) {
-            heading.innerText = "This Week's Tasks"
-            
-        }
+  function toggleTodoForm(e) {
+    if (e.target.classList.contains("add-todo")) {
+      e.target.classList.add("hide");
+      var form = document.querySelector(".todo-form");
+      form.classList.remove("hide");
+    } else if (e.target.classList.contains("cancel-todo")) {
+      var form = document.querySelector(".todo-form");
+
+      form.classList.add("hide");
+
+      var todoButton = document.querySelector(".add-todo");
+
+      todoButton.classList.remove("hide");
     }
-
-    function toggleProjectForm(e){
-        if (e.target.classList.contains('add-project')) {
-            e.target.classList.add('hide');
-            var form = document.querySelector('.project-form');
-            form.classList.remove('hide')
-        }
-        else if (e.target.classList.contains('cancel-project')) {
-            var form = document.querySelector('.project-form');
-
-            form.classList.add('hide');
-            var input = document.querySelector('.project-name-inp');
-            input.value = '';
-            var projectButton = document.querySelector('.add-project');
-
-            projectButton.classList.remove('hide')
-        }
-    }
-
-    function toggleTodoForm(e){
-        if (e.target.classList.contains('add-todo')) {
-            e.target.classList.add('hide');
-            var form = document.querySelector('.todo-form');
-            form.classList.remove('hide')
-        }
-        else if (e.target.classList.contains('cancel-todo')) {
-            var form = document.querySelector('.todo-form');
-
-            form.classList.add('hide');
-            
-            var todoButton = document.querySelector('.add-todo');
-
-            todoButton.classList.remove('hide')
-        }
-    }
-})()
+  }
+})();
