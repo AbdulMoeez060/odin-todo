aca8918377aa89bd74a4bee42924f781aa01bc62
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-10-07 14:08:17 +0500

    add event listeners to add project utton

--- src/index.js
+++ src/index.js
@@ -7,3 +7,40 @@ import week from "./assets/week.png";
 import add from "./assets/add.png";
 import del from "./assets/del.png";
 import drag from "./assets/drag.png";
+
+
+const displayController = (()=>{
+
+    addEvents();
+    function addEvents() {
+        var menu = document.querySelector('.hamburger');
+        var addProjectButton = document.querySelector('.add-project');
+        var projectForm = document.querySelector('.project-form');
+
+        menu.addEventListener('click',clickMenu);
+        addProjectButton.addEventListener('click',toggleProjectForm);
+        projectForm.addEventListener('click',toggleProjectForm);
+    }
+
+
+    function clickMenu(){
+        var sidebar = document.querySelector('.sidebar')
+        sidebar.classList.toggle('hide');
+    }
+
+    function toggleProjectForm(e){
+        if (e.target.classList.contains('add-project')) {
+            e.target.classList.add('hide');
+            var form = document.querySelector('.project-form');
+            form.classList.remove('hide')
+        }
+        else if (e.target.classList.contains('cancel-project')) {
+            var form = document.querySelector('.project-form');
+
+            form.classList.add('hide');
+            var projectButton = document.querySelector('.add-project');
+
+            projectButton.classList.remove('hide')
+        }
+    }
+})()
